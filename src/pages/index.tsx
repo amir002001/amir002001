3e9adc3b5a490a29146508e986e2f6e7b4f2665a
Amir Afshari <a_amirhossein@icloud.com>
2022-11-16 11:21:23 -0500

    section quote added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,45 +3,48 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import DotsSquare from "../components/svgs/DotsSquare";
+import { VscQuote } from "react-icons/vsc";
 import LinesRightAngle from "../components/svgs/LinesRightAngle";
 
-import { trpc } from "../utils/trpc";
+// import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-  
+
   return (
     <>
       <Head>
         <title>Home - Amir</title>
-        
       </Head>
 
       <main className="container mx-auto p-4">
         {/* navbar */}
         <Navbar></Navbar>
         {/* hero */}
-        <section id="hero" className="relative w-full h-[90vh] flex flex-col justify-between mt-2
-        md:flex-row">
+        <section
+          id="hero"
+          className="relative mt-2 flex h-[90vh] w-full flex-col justify-between
+        md:flex-row"
+        >
           {/* holds title and subtitle */}
           <div className="md:absolute md:top-1/4">
-            <h1 className="text-3xl font-medium max-w-md">
+            <h1 className="max-w-md text-3xl font-medium">
               I am a <span className="text-primary">software engineer</span> and
               I <span className="text-primary">solve problems</span>
             </h1>
-            <h4 className="mt-6 text-gray-400 max-w-md">
+            <h4 className="mt-6 max-w-md text-gray-400">
               I love new tech. Talk to me about serverless, lambda, and anything
               JavaScript
             </h4>
           </div>
-          {/* hold s currently working and photo */}
-          <div className="md:absolute md:right-0 md:top-1/2 md:-translate-y-1/2">
+          {/* holds currently working and photo */}
+          <div className="md:absolute md:right-0 md:top-1/3 md:-translate-y-1/2">
             {/* image */}
             <div className="relative w-full">
               <div className="absolute bottom-20 left-1/4 w-24">
                 <LinesRightAngle />
               </div>
-              <div className="relative w-[calc(100vw/3)] h-[calc(100vh/3)] mx-auto max-w-md brightness-0">
+              <div className="relative mx-auto h-[calc(100vh/3)] w-[calc(100vw/3)] max-w-md brightness-0">
                 <Image
                   src={"/temp.png"}
                   alt="me"
@@ -63,7 +66,29 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
+          {/* holds quote and cite and icons */}
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 max-md:hidden">
+            <div className="relative flex h-full w-full flex-col items-end p-2">
+              {/* quote */}
+              <div className="border border-gray-400 p-8">
+                <h2>
+                  Learn from yesterday, live for today, hope for tomorrow.
+                </h2>
+              </div>
+              {/* cite */}
+              <div className="border border-t-0 border-gray-400 p-4">
+                <h4>Albert Einstein</h4>
+              </div>
+              <div className="absolute left-5 -translate-y-1/2 bg-bggray text-gray-400 p-1">
+                <VscQuote />
+              </div>
+              <div className="absolute bottom-[42px] right-6 -translate-y-1/2 bg-bggray p-1 text-gray-400">
+                <VscQuote />
+              </div>
+            </div>
+          </div>
         </section>
+        {/*  */}
       </main>
     </>
   );
